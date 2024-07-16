ceacf8fea317ec325c10e228c85f53cddd7ae99d
Alex Soares <166084904+AlexSoares98@users.noreply.github.com>
2024-07-16 10:29:02 -0300

    Terreno.java

--- modelo/Terreno.java
+++ modelo/Terreno.java
@@ -3,7 +3,6 @@ package modelo;
 public class Terreno extends Financiamento{
     private String tipoZona;
 
-    // Construtor que inicializa os atributos do financiamento
     public Terreno(double valorDesejadoImovel, int prazoFinanciamentoAnos, double taxaJurosAnual, String tipoZona) {
         super(valorDesejadoImovel, prazoFinanciamentoAnos, taxaJurosAnual);
         this.tipoZona = tipoZona;
@@ -11,15 +10,11 @@ public class Terreno extends Financiamento{
 
     @Override
     public double calcularPagamentoMensal() {
-        // Calcula a taxa mensal
         double taxaMensal = this.taxaJurosAnual / 12 / 100;
-        // Calcula o valor em meses do financiamento
         int mesesFinanciamento = this.prazoFinanciamento * 12;
-        // Calculo do pagamento mensal base
         return (super.getValorImovel() / mesesFinanciamento) * (1 + (taxaMensal)) * 1.02;
     }
 
-    // Descrição salva sobre o financiamento do terreno
     @Override
     public String toString() {
         return String.format("""
