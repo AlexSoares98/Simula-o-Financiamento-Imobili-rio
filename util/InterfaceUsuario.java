0c58d0d16459e93222774992a0e5eba68d6d962c
Alex Soares <166084904+AlexSoares98@users.noreply.github.com>
2024-07-16 10:28:33 -0300

    InterfaceUsuario.java

--- util/InterfaceUsuario.java
+++ util/InterfaceUsuario.java
@@ -1,8 +1,3 @@
-/* Aluno: Alex Augusto Soares
- * Curso: Análise e Desenvolvimento de Sistemas
- * Faculdade: PUCPPR
- */
-
 package util;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -10,7 +5,6 @@ import java.util.Scanner;
 public class InterfaceUsuario {
     Scanner scanner = new Scanner(System.in);
 
-    // Solicita ao usuário o valor do imóvel e o código utiliza o tratamento de exceções
     public double pedirValorImovel() {
         double valorImovel;
         while (true) {
@@ -18,7 +12,7 @@ public class InterfaceUsuario {
                 System.out.print("Digite o valor do imóvel (Apenas números): ");
                 valorImovel = scanner.nextDouble();
                 if (valorImovel <= 0) {
-                    throw new IllegalArgumentException("Valor do imóvel deve ser positivo! Digite novamente."); // Mensagem de erro, caso o valor digitado seja inválido
+                    throw new IllegalArgumentException("Valor do imóvel deve ser positivo! Digite novamente."); 
                 }
                 break;
             } catch (InputMismatchException e) {
@@ -31,7 +25,6 @@ public class InterfaceUsuario {
         return valorImovel;
     }
 
-    // Solicita ao usuário o prazo do financiamento em anos e o código utiliza o tratamento de exceções
     public int pedirPrazoFinanciamento() {
         int prazoFinanciamento;
         while (true) {
@@ -39,10 +32,10 @@ public class InterfaceUsuario {
                 System.out.print("Digite o prazo do financiamento (Em anos): ");
                 prazoFinanciamento = scanner.nextInt();
                 if (prazoFinanciamento <= 0) {
-                    throw new IllegalArgumentException("O prazo do financiamento deve ser positivo! Digite novamente."); // Mensagem de erro, caso o valor digitado seja inválido
+                    throw new IllegalArgumentException("O prazo do financiamento deve ser positivo! Digite novamente."); 
                 }
                 else if (prazoFinanciamento > 35) {
-                    throw new IllegalArgumentException("O prazo máximo permitido é de 35 anos! Digite novamente."); // Mensagem de erro, caso o valor digitado seja inválido
+                    throw new IllegalArgumentException("O prazo máximo permitido é de 35 anos! Digite novamente."); 
                 }
                 break;
             } catch (InputMismatchException e) {
@@ -55,7 +48,6 @@ public class InterfaceUsuario {
         return prazoFinanciamento;
     }
 
-    // Solicita ao usuário a taxa de juros do financiamento e o código utiliza o tratamento de exceções
     public double pedirTaxaJuros() {
         double taxaJuros;
         while (true) {
@@ -63,10 +55,10 @@ public class InterfaceUsuario {
                 System.out.print("Digite a taxa de juros anual do financiamento (Sem %): ");
                 taxaJuros = scanner.nextDouble();
                 if (taxaJuros <= 0) {
-                    throw new IllegalArgumentException("A taxa de juros deve ser positiva! Digite novamente."); // Mensagem de erro, caso o valor digitado seja inválido
+                    throw new IllegalArgumentException("A taxa de juros deve ser positiva! Digite novamente."); 
                 }
                 else if (taxaJuros > 12) {
-                    throw new IllegalArgumentException("A taxa de juros não pode ultrapassar 12% ano ano (Lei nº 1.521/51). Digite novamente!"); // Mensagem de erro, caso o valor digitado seja inválido
+                    throw new IllegalArgumentException("A taxa de juros não pode ultrapassar 12% ano ano (Lei nº 1.521/51). Digite novamente!"); 
                 }
                 break;
             } catch (InputMismatchException e) {
@@ -79,7 +71,6 @@ public class InterfaceUsuario {
         return taxaJuros;
     }
 
-    // Solicita ao usuário a área a ser construída no terreno e o código utiliza o tratamento de exceções
     public double pedirAreaConstruida() {
         double areaConstruida;
         while (true) {
@@ -100,7 +91,6 @@ public class InterfaceUsuario {
         return areaConstruida;
     }
 
-    // Solicita ao usuário a área do terreno e o código utiliza o tratamento de exceções
     public double pedirAreaTerreno(double areaConstruida) {
         double areaTerreno;
         while (true) {
@@ -111,7 +101,7 @@ public class InterfaceUsuario {
                     throw new IllegalArgumentException("A área do terreno deve ser positiva! Digite novamente.");
                 }
                 else if (areaTerreno < areaConstruida) {
-                    throw new IllegalArgumentException("A área do terreno deve ser maior ou igual a área construída. Digite novamente!"); // Mensagem de erro, caso o valor digitado seja inválido
+                    throw new IllegalArgumentException("A área do terreno deve ser maior ou igual a área construída. Digite novamente!"); 
                 }
                 break;
             } catch (InputMismatchException e) {
@@ -125,9 +115,6 @@ public class InterfaceUsuario {
     }
 
 
-    // Alguns métodos abaixo não estão sendo utilizados, mas foram feitos por garantia de um uso futuro
-
-    // Solicita ao usuário o número de vagas na garagem e o código utiliza o tratamento de exceções
     public int pedirNumeroVagasGaragem() {
         int numeroVagas;
         while (true) {
@@ -138,7 +125,7 @@ public class InterfaceUsuario {
                     throw new IllegalArgumentException("O número de vagas na garagem deve ser positivo ou zero! Digite novamente.");
                 }
                 else if (numeroVagas > 20) {
-                    throw new IllegalArgumentException("O número de vagas na garagem está indisponível! Digite novamente."); // Mensagem de erro, caso o valor digitado seja inválido
+                    throw new IllegalArgumentException("O número de vagas na garagem está indisponível! Digite novamente."); 
                 }
                 break;
             } catch (InputMismatchException e) {
@@ -151,7 +138,6 @@ public class InterfaceUsuario {
         return numeroVagas;
     }
 
-    // Solicita ao usuário o número do andar do prédio e o código utiliza o tratamento de exceções
     public int pedirNumeroAndar() {
         int numeroAndar;
         while (true) {
@@ -162,7 +148,7 @@ public class InterfaceUsuario {
                     throw new IllegalArgumentException("O número do andar deve ser positivo ou zero (Térreo)! Digite novamente.");
                 }
                 else if (numeroAndar > 80) {
-                    throw new IllegalArgumentException("Número do andar indisponível! Digite novamente."); // Mensagem de erro, caso o valor digitado seja inválido
+                    throw new IllegalArgumentException("Número do andar indisponível! Digite novamente."); 
                 }
                 break;
             } catch (InputMismatchException e) {
@@ -175,7 +161,6 @@ public class InterfaceUsuario {
         return numeroAndar;
     }
 
-    // Solicita ao usuário o tipo de zona do terreno (residencial ou comercial) e o código utiliza o tratamento de exceções
     public String pedirTipoZona() {
         String tipoZona;
         while (true) {
